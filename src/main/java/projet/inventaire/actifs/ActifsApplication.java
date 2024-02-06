3072c38c1be2dc012f72f9b9c8e7f6300645e7c8
edgard1602 <edgardpadasse@gmail.com>
2024-02-06 15:28:48 +0000

    premier commit entite

---
+++ src/main/java/projet/inventaire/actifs/ActifsApplication.java
@@ -0,0 +1,13 @@
+package projet.inventaire.actifs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActifsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActifsApplication.class, args);
+	}
+
+}
